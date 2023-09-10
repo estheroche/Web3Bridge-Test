75efd05f3e4d6216b846c2450c0d0b5d8841dde8
estheroche <you@example.com>
2023-09-10 17:47:41 -0500

    changes

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -1,107 +1,159 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.19;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./TokenA.sol";
-import "./TokenB.sol";
 
-contract Swap {
+contract TokenSwapCPMM {
+    IERC20 public tokenA;
+    IERC20 public tokenB;
+    uint256 public totalLiquidity;
+    uint256 public constant k = 1000; // CPMM constant, adjust as needed
+
     struct LiquidityProvider {
-        uint amount1;
-        uint amount2;
+        uint256 amount1;
+        uint256 amount2;
     }
 
-    IERC20 TokenA;
-    IERC20 TokenB;
+    mapping(address => LiquidityProvider) public liquidityProviders;
+
+    event TokensSwapped(
+        address indexed user,
+        uint256 amountA,
+        uint256 amountB,
+        uint256 amountAReceived,
+        uint256 amountBReceived
+    );
+    event LiquidityAdded(
+        address indexed provider,
+        uint256 amountA,
+        uint256 amountB
+    );
+    event LiquidityWithdrawn(
+        address indexed provider,
+        uint256 amountA,
+        uint256 amountB
+    );
 
     constructor(IERC20 _tokenA, IERC20 _tokenB) {
-        TokenA = IERC20(_tokenA);
-        TokenB = IERC20(_tokenB);
+        tokenA = _tokenA;
+        tokenB = _tokenB;
     }
 
-    mapping(address => LiquidityProvider) public _liquidityProvider;
-    mapping(address => uint) public  balance;
+    function addLiquidity(uint256 amountA, uint256 amountB) external {
+        require(
+            amountA > 0 && amountB > 0,
+            "Both amounts must be greater than zero"
+        );
 
-    function addLiquidity(uint256 amount1, uint256 amount2) external {
-        uint provider; // counter
-        LiquidityProvider storage ego = _liquidityProvider[msg.sender];
-        require(this.balance >= amount1, "ERC20 insuficient balance");
-        ego.amount1 += amount1;
-        ego.amount2 += amount2;
-        uint reserveA;
-        uint reserveB;
+        uint256 poolBalanceA = tokenA.balanceOf(address(this));
+        uint256 poolBalanceB = tokenB.balanceOf(address(this));
 
-        bool status = IERC20(TokenA).transferFrom(
+        if (totalLiquidity == 0 || poolBalanceA == 0 || poolBalanceB == 0) {
+            require(
+                tokenA.transferFrom(msg.sender, address(this), amountA),
+                "Transfer of Token A failed"
+            );
+            require(
+                tokenB.transferFrom(msg.sender, address(this), amountB),
+                "Transfer of Token B failed"
+            );
+        } else {
+            uint256 liquidityMinted = (amountA * totalLiquidity) / poolBalanceA;
+            require(liquidityMinted > 0, "Insufficient liquidity to mint");
+
+            uint256 newLiquidity = totalLiquidity + liquidityMinted;
+            require(
+                tokenA.transferFrom(msg.sender, address(this), amountA),
+                "Transfer of Token A failed"
+            );
+            require(
+                tokenB.transferFrom(msg.sender, address(this), amountB),
+                "Transfer of Token B failed"
+            );
+
+            liquidityProviders[msg.sender].amount1 +=
+                (amountA * totalLiquidity) /
+                poolBalanceA;
+            liquidityProviders[msg.sender].amount2 +=
+                (amountB * totalLiquidity) /
+                poolBalanceB;
+            totalLiquidity = newLiquidity;
+        }
+
+        emit LiquidityAdded(msg.sender, amountA, amountB);
+    }
+
+    function removeLiquidity(uint256 liquidity) external {
+        require(
+            liquidity > 0 &&
+                liquidityProviders[msg.sender].amount1 >= liquidity &&
+                liquidityProviders[msg.sender].amount2 >= liquidity,
+            "Invalid liquidity amount"
+        );
+
+        uint256 poolBalanceA = tokenA.balanceOf(address(this));
+        uint256 poolBalanceB = tokenB.balanceOf(address(this));
+
+        uint256 amountA = (liquidity * poolBalanceA) / totalLiquidity;
+        uint256 amountB = (liquidity * poolBalanceB) / totalLiquidity;
+
+        require(
+            tokenA.transfer(msg.sender, amountA),
+            "Transfer of Token A failed"
+        );
+        require(
+            tokenB.transfer(msg.sender, amountB),
+            "Transfer of Token B failed"
+        );
+
+        liquidityProviders[msg.sender].amount1 -= amountA;
+        liquidityProviders[msg.sender].amount2 -= amountB;
+        totalLiquidity -= liquidity;
+
+        emit LiquidityWithdrawn(msg.sender, amountA, amountB);
+    }
+
+    function swapTokens(uint256 amountA, uint256 amountB) external {
+        require(
+            amountA > 0 && amountB > 0,
+            "Both amounts must be greater than zero"
+        );
+
+        uint256 poolBalanceA = tokenA.balanceOf(address(this));
+        uint256 poolBalanceB = tokenB.balanceOf(address(this));
+        uint256 amountAReceived = (amountB * poolBalanceA) /
+            (poolBalanceB + amountB);
+        uint256 amountBReceived = (amountA * poolBalanceB) /
+            (poolBalanceA + amountA);
+
+        require(
+            amountAReceived > 0 && amountBReceived > 0,
+            "Swap would result in zero tokens received"
+        );
+
+        require(
+            tokenA.transferFrom(msg.sender, address(this), amountA),
+            "Transfer of Token A failed"
+        );
+        require(
+            tokenB.transferFrom(msg.sender, address(this), amountB),
+            "Transfer of Token B failed"
+        );
+        require(
+            tokenA.transfer(msg.sender, amountAReceived),
+            "Transfer of Token A to user failed"
+        );
+        require(
+            tokenB.transfer(msg.sender, amountBReceived),
+            "Transfer of Token B to user failed"
+        );
+
+        emit TokensSwapped(
             msg.sender,
-            address(this),
-            amount1
+            amountA,
+            amountB,
+            amountAReceived,
+            amountBReceived
         );
-        require(status == true, "transfer Failed");
-        IERC20(TokenB).transferFrom(msg.sender, address(this), amount2);
-    }
-
-    function removeLiquidity(
-        address tokenA,
-        address tokenB,
-        uint liquidity,
-        uint amountAMin,
-        uint amountBMin,
-        address to,
-        uint deadline
-    ) external returns (uint amountA, uint amountB);
-
-    function withdrawA(uint amount) external {
-        uint LiquidityProvided = _liquidityProvider[msg.sender];
-        require(balance.[msg.sender] >= amount, "insufficent liquidity amount");
-        LiquidityProvider storage ego = _liquidityProvider[msg.sender];
-        ego.amount2 -= amount;
-        TokenA.transfer(msg.sender, amount);
-    }
-
-    function withdrawB(uint amount) external {
-        uint LiquidityProvided = _liquidityProvider(msg.sender);
-        require(_liquidityProvider >= amount, "insufficent liquidity amount");
-        LiquidityProvider storage ego = _liquidityProvider(msg.sender);
-        ego.amount2 -= amount;
-        TokenB.transfer(msg.sender, amount);
-    }
-
-    // // /*
-    //  * Convert an amount of input token_ to an equivalent amount of the output token
-    //  token_ address of token to swap
-    //  amount amount of token to swap/receive
-    //  */
-    function swapp(address token_, uint256 amount) private {
-        require(
-            IERC20(token_).transferFrom(msg.sender, address(this), amount),
-            "ERC20: Error on transfer"
-        );
-        require(TokenB.transfer(msg.sender, amount), "Error");
-    }
-
-    /*
-     * Convert an amount of the output token to an equivalent amount of input token_
-     token_ address of token to receive
-     amount amount of token to swap/receive
-     */
-    function _unswap(address token_, uint256 amount) private {
-        require(
-            TokenB.transferFrom(msg.sender, address(this), amount),
-            "ERC20: Error on transfer"
-        );
-        require(
-            IERC20(token_).transfer(msg.sender, amount),
-            "ERC20: Error on transfer"
-        );
-    }
-
-    function transferToken(
-        IERC20 tokenToTransfer,
-        address from,
-        address to,
-        uint256 amount
-    ) private {
-        bool sent = tokenToTransfer.transferFrom(from, to, amount);
-        require(sent, "Failed");
     }
 }
