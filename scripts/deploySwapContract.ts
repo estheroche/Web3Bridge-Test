eae6143190be79c0bf83f0c62f0f506d04ee90e4
estheroche <you@example.com>
2023-09-11 08:55:25 -0500

    IMPROVEEEE

--- scripts/deploySwapContract.ts
+++ scripts/deploySwapContract.ts
@@ -3,7 +3,7 @@ import { ethers } from 'hardhat'
 async function main() {
   const Swap = await ethers.deployContract("TokenSwap", [
     "0xbC72a571bcdDB3086823F5FA117E95E639EfACd4",
-    "0xD6C4A6a1d9A0Ddf25431881a2EBF75609A523882"]);
+    "0x59dF163602D9f16587FB3FABbA5AA13b88eF37b7"]);
   Swap.waitForDeployment();
 
   console.log(Swap.target);
@@ -17,6 +17,6 @@ main().catch((error) => {
 });
 
 // TokenA.=	0xbC72a571bcdDB3086823F5FA117E95E639EfACd4
-// TokenB.= 0xD6C4A6a1d9A0Ddf25431881a2EBF75609A523882
+// TokenB.	= 0xD6C4A6a1d9A0Ddf25431881a2EBF75609A523882
 
 // SwapAddr = 0xa929dFDB43Cd2C9Ddeb60B39Ec387662a69e78e6
